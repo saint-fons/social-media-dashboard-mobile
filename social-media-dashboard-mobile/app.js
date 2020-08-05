31728ee9a0166c37607dc5bd6d436348af4fedd7
saint-fons <62059885+saint-fons@users.noreply.github.com>
2020-08-05 09:54:08 +0300

    Add files via upload

---
+++ social-media-dashboard-mobile/app.js
@@ -0,0 +1,7 @@
+ function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+     var element1 = document.overview;
+     element1.classList.toggle("dark-mode");
+}
